fdc71ce59897d8e6b9a603b2a4c023f0c666d5e1
pleximaDev <41956118+pleximaDev@users.noreply.github.com>
2021-05-17 16:30:35 +0300

    Create solution.c

---
+++ String_to_integer/solution.c
@@ -0,0 +1,34 @@
+int myAtoi(char * s)
+{
+    int integer = 0;
+    
+    if(*s == NULL) return 0;
+    
+    for(int i = 0 ; ; i++)
+    {
+        if(*(s + i) >= '0' && *(s + i) <= '9')
+        {
+            if(integer > 100000000 || integer < -100000000)
+            {
+                if((*s == '-' ? -((double)integer * 10 + *(s + i) - '0') : (double)integer * 10 + *(s + i) - '0') <= INT_MIN) return INT_MIN;
+                else if((*s == '-' ? -((double)integer * 10 + *(s + i) - '0') : (double)integer * 10 + *(s + i) - '0') >= INT_MAX) return INT_MAX;
+            }
+
+            integer = integer * 10 + (*(s + i) - '0');
+            
+            if(*(s + 1) != '`' && i >= 1){*(s + 1) = '`';}
+            else if(*(s + 1) != '`' && *(s) != '`')*(s) = '`';
+            
+        }
+        else if(integer == 0 && *(s + 1) != '`' && (*s != '`') && *(s + i) == ' '
+                ||
+                (*(s + i) == '-' && (*(s + 1) != '`') && (*(s) != '`') && (*(s + i + 1) >= '0' && *(s + i + 1) <= '9'))
+                ||
+                (*(s + i) == '+' && (*(s + 1) != '`') && (*(s) != '`') && (*(s + i + 1) >= '0' && *(s + i + 1) <= '9')))
+        {if(*(s + i) == '-')*(s) = '-';continue;}
+        else
+        {
+            return integer == 0 ? integer : *s == '-' ? -integer : integer;
+        }
+    }
+}
