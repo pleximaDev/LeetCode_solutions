ae4840877757c3507041df69a41306ef5e3fbdf2
pleximaDev <41956118+pleximaDev@users.noreply.github.com>
2021-06-27 15:18:10 +0300

    Update solution.py

--- Power_of_Two/solution.py
+++ Power_of_Two/solution.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/power-of-two/
+# one-liner
+# faster than 86.58%, 28 ms
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         return bool(n) if n in (0,1) else False if n<0 else not bool(n&(n-1))
